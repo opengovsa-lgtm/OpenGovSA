8c548919d10f2c0a9fd0fe6fdd8a3524cc8f8d7c
fenceitalldurban-tech <opengovsa@gmail.com>
2026-07-11 01:47:20 +0300

    fix(style): apply isort formatting to alembic env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,12 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
-from app.core.database import Base
 from app.core.config import settings
+from app.core.database import Base
 
 config = context.config
 
