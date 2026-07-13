1d8841e694eb44c8749bafe51db511fd30dbeb94
fenceitalldurban-tech <opengovsa@gmail.com>
2026-07-13 19:38:03 +0300

    fix(style): remove blank line between third-party and local imports per isort

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
-
 from alembic import context
+
 from app.core.config import settings
 from app.core.database import Base
 
